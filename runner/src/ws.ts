70bba5ac12ebe560ea92aaee9f6cd0dd573806fa
Ayush Srivastava <ayushsrisks@gmail.com>
2025-07-13 23:01:18 +0530

    feat: added websocket persistent connection

--- runner/src/ws.ts
+++ runner/src/ws.ts
@@ -1,3 +1,76 @@
 import { Server, Socket } from "socket.io";
-import {Server as HttpServer} from "http";
-export function initWs(httpServer: HttpServer) {}
+import { Server as HttpServer } from "http";
+import { saveToS3 } from "./aws"
+import path from "path";
+import { fetchDir, fetchFileContent, saveFile } from "./fs";
+import { TerminalManager } from "./pty";
+
+const terminalManager = new TerminalManager();
+
+export function initWs(httpServer: HttpServer) {
+    const io = new Server(httpServer, {
+        cors: {
+            origin: "*",
+            methods: ["GET", "POST"],
+        },
+    });
+      
+    io.on("connection", async (socket) => {
+        // auth check
+        const host = socket.handshake.headers.host;
+        console.log(`host is ${host}`);
+        // Split the host by '.' and take the first part as execId
+        const execId = host?.split('.')[0];
+    
+        if (!execId) {
+            socket.disconnect();
+            terminalManager.clear(socket.id);
+            return;
+        }
+
+        socket.emit("loaded", {
+            rootContent: await fetchDir("/workspace", "")
+        });
+
+        initHandlers(socket, execId);
+    });
+}
+
+function initHandlers(socket: Socket, execId: string) {
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
+
+    socket.on("fetchDir", async (dir: string, callback) => {
+        const dirPath = `/workspace/${dir}`;
+        const contents = await fetchDir(dirPath, dir);
+        callback(contents);
+    });
+
+    socket.on("fetchContent", async ({ path: filePath }: { path: string }, callback) => {
+        const fullPath = `/workspace/${filePath}`;
+        const data = await fetchFileContent(fullPath);
+        callback(data);
+    });
+
+   //needs fixing
+    socket.on("updateContent", async ({ path: filePath, content }: { path: string, content: string }) => {
+        const fullPath =  `/workspace/${filePath}`;
+        await saveFile(fullPath, content);
+        await saveToS3(`code/${execId}`, filePath, content);
+    });
+
+    socket.on("requestTerminal", async () => {
+        terminalManager.createPty(socket.id, execId, (data, id) => {
+            socket.emit('terminal', {
+                data: Buffer.from(data,"utf-8")
+            });
+        });
+    });
+    
+    socket.on("terminalData", async ({ data }: { data: string, terminalId: number }) => {
+        terminalManager.write(socket.id, data);
+    });
+
+}
